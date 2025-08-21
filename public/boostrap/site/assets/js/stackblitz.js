a7aa5fdfb50f4fe27de65b134b292e821a71eb82
Shayla del Carmen Garcia Mora <dsm23190051.sgarcia@alumnos.utsv.edu.mx>
2025-08-21 14:25:09 -0600

    corregir rutas

--- public/boostrap/site/assets/js/stackblitz.js
+++ public/boostrap/site/assets/js/stackblitz.js
@@ -11,26 +11,26 @@
  * For details, see https://creativecommons.org/licenses/by/3.0/.
  */
 
-import sdk from '@stackblitz/sdk'
+import sdk from "@stackblitz/sdk";
 // https://gohugo.io/hugo-pipes/js/#options
-import {
-  cssCdn, docsVersion, jsBundleCdn, jsSnippetFile
-} from '@params' // eslint-disable-line import/no-unresolved
+import { cssCdn, docsVersion, jsBundleCdn, jsSnippetFile } from "@params"; // eslint-disable-line import/no-unresolved
 
 // Open in StackBlitz logic
-document.querySelectorAll('.btn-edit').forEach(btn => {
-  btn.addEventListener('click', event => {
-    const codeSnippet = event.target.closest('.bd-code-snippet')
-    const exampleEl = codeSnippet.querySelector('.bd-example')
+document.querySelectorAll(".btn-edit").forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const codeSnippet = event.target.closest(".bd-code-snippet");
+    const exampleEl = codeSnippet.querySelector(".bd-example");
 
-    const htmlSnippet = exampleEl.innerHTML
-    const jsSnippet = codeSnippet.querySelector('.btn-edit').getAttribute('data-sb-js-snippet')
+    const htmlSnippet = exampleEl.innerHTML;
+    const jsSnippet = codeSnippet
+      .querySelector(".btn-edit")
+      .getAttribute("data-sb-js-snippet");
     // Get extra classes for this example
-    const classes = Array.from(exampleEl.classList).join(' ')
+    const classes = Array.from(exampleEl.classList).join(" ");
 
-    openBootstrapSnippet(htmlSnippet, jsSnippet, classes)
-  })
-})
+    openBootstrapSnippet(htmlSnippet, jsSnippet, classes);
+  });
+});
 
 const openBootstrapSnippet = (htmlSnippet, jsSnippet, classes) => {
   const indexHtml = `<!doctype html>
@@ -41,26 +41,30 @@ const openBootstrapSnippet = (htmlSnippet, jsSnippet, classes) => {
     <link href="${cssCdn}" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/${docsVersion}/assets/css/docs.css" rel="stylesheet">
     <title>Bootstrap Example</title>
-    <${'script'} defer src="${jsBundleCdn}"></${'script'}>
+    <${"script"} defer src="${jsBundleCdn}"></${"script"}>
   </head>
   <body class="p-3 m-0 border-0 ${classes}">
     <!-- Example Code Start-->
-${htmlSnippet.trimStart().replace(/^/gm, '    ').replace(/^ {4}$/gm, '').trimEnd()}
+${htmlSnippet
+  .trimStart()
+  .replace(/^/gm, "    ")
+  .replace(/^ {4}$/gm, "")
+  .trimEnd()}
     <!-- Example Code End -->
   </body>
 </html>
-`
+`;
 
   const project = {
     files: {
-      'index.html': indexHtml,
-      ...(jsSnippet && { 'index.js': jsSnippetFile })
+      "index.html": indexHtml,
+      ...(jsSnippet && { "index.js": jsSnippetFile }),
     },
-    title: 'Bootstrap Example',
+    title: "Bootstrap Example",
     description: `Official example from ${window.location.href}`,
-    template: jsSnippet ? 'javascript' : 'html',
-    tags: ['bootstrap']
-  }
+    template: jsSnippet ? "javascript" : "html",
+    tags: ["bootstrap"],
+  };
 
-  sdk.openProject(project, { openFile: 'index.html' })
-}
+  sdk.openProject(project, { openFile: "index.html" });
+};
