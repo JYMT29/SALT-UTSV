b3a879e608bb21e5cdb2bee84a523754a6e5e451
Shayla del Carmen Garcia Mora <dsm23190051.sgarcia@alumnos.utsv.edu.mx>
2025-09-01 02:53:47 -0600

    Cambio de puerto 3000 a railway

--- public/js/registrar-laboratorio.js
+++ public/js/registrar-laboratorio.js
@@ -42,6 +42,6 @@ app.post("/registrar-lab", (req, res) => {
 });
 
 // Iniciar el servidor
-app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
-});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
